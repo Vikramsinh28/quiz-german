bb9fc46ca01ebbfd60b93ae3ac5fb691d522e05f
vikram@281201 <vikramsinhparmar2812@gmail.com>
2025-11-27 00:58:56 +0530

    Revert "Update Firebase service account configuration"
    This reverts commit b652436d52919b8684735c9e4123f1ed55425054.

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,17 +5,17 @@ let firebaseInitialized = false;
 try {
     // Firebase service account configuration
     const serviceAccount = {
-          "type": "service_account",
-          "project_id": "quiz-german",
-          "private_key_id": "93a49ef0b7e627af0171e0ee8baf0baf3c05c8c8",
-          "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCVPN6jH/ySjq2Q\nTQysrKIKsM+qhsD5HOLVXNSMMnWGJjTdiaB5P6x91mYXj5/xLOMHl+2Fz1w8w3Kd\nzq+EZLw9VizQ+5W7WpufVyXD74lmxIrZ72hSnMfHdqch/HtVPnRY5ofBHqHh8a/D\nascOhRHxEvbbzZBkMdTuGtNcwbVnTcXPyJiNBJz14/ZND18WRAmRy98cQZ9yzQl0\nTBBCZKLz6jyVj8C27s57WTBk22hqktYtpPByHZDFEj/qh0OupRWLKV+Zbky0RkVl\nwCzx1TR3FhEg5NrzMDjqgyJSAb16XImFhiB/AnAJ5ngqXAg4iYtSmxzQcEQyoh/N\nAwqZQ2iLAgMBAAECggEAPQdh4mKTxpk7SA4CN2D3eRi6K7mkk5ro9ijasKR35A6e\nwtPjwIG199Xj9RGibh2VkAbktrpINX8V0YvywdjyE8E4XZPstOvGRUB9rKU50sFg\nKORbDKGZnyuaCG94PjFmoe5QKLzupXfREUCKu5jWb/f55IQpXpf0RBTLR5zdoqsO\n5W+t0fwDK55bQo/99WKLJaasmpXwXshJ9O4X2aJ7aatTzmNkItynxhMWapCOb2d+\nDU4Ch74fcMB0HP4dcKCKZrltP885ZDz1iX+lG0691qjwCIRy9234Fu73E5VhLkWj\nR+3w16a0kvkW3CV1t9MFlAQ/skskDlB92Ts3T2gMfQKBgQDIiddtIue6g1jYy5e9\nyUDDbP9lv7eBVayX1fwtBK5wNt9EhTb6+ucLjb6Tl2MYozEM9PWlC+2gIdsxF1De\n7VprMbOq3UGfvMNmSc0MSKARaSK96RPIJ9pdrQiq1WvJsNmMAIzDNoxbb0ax1Scn\nAf0BFHl2tdNIRiSeUy4phriXLwKBgQC+gu5m3W6CnXmS9WmfLTpxDfTpCDpUZpq8\nj9D7KEEuIz9i283EC8+lhRnYlmcQvYoNm0fQWNxVdfQh98fFlPwPrhGdb3FsuFSe\nKQxy93z3j2SBbmvUzq3g4ue8yVFgIn2o5ac0WzsIQtMltnLBJ1XE+SI/o3tB1yyk\nKEVKzsutZQKBgHWNn9Og33fM4PsMtWGSJL2qGjh53scZArhqvo1hLiQndk+DS4xq\nIZ/7MhjwaC2cE/fFbG2YhW4WCphHU6eqOROXKfviAULy8xP19RXbSMGMAMqUY300\nJ2Q8N1V1D99GJPBVri4MvtAmdalvCqlqF/ZXGzCEO1sliveoSOocx/4PAoGARrJP\nxdxmJhJKCz8G8wrVpR6Iw/FJtAhfY5n68zj89yIdY5ItyZKYe9Pow3cMyCmOEX6O\nJ8LSAOTEs9Y/TBL9dtQhtUqlzLKatBaHiCJPPY74vQ889Lu9yrZpSKVsjUA/YW3X\nnpY8UdR6hoQuEX+HVJEfJH6GPuiaqFBJZnlXHAECgYEAkPjY4xjCiXGGKm5UAdWf\n3luq+h5h9sHTghrbfhxwshrIEptMfzvdnr1gTk+7peNk+5WF9XY+cvPS0CaPRwSK\nOfWtWbPZ2Gk9o2qjT79W+LjdY3q86vae/UBOi8pm8+vFhbnYdLMCky386yem0YnE\npPQol9H0PSB8wXXlLchXSkw=\n-----END PRIVATE KEY-----\n",
-          "client_email": "firebase-adminsdk-fbsvc@quiz-german.iam.gserviceaccount.com",
-          "client_id": "112664334990274609578",
-          "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-          "token_uri": "https://oauth2.googleapis.com/token",
-          "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-          "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40quiz-german.iam.gserviceaccount.com",
-          "universe_domain": "googleapis.com"
+        "type": "service_account",
+        "project_id": "quiz-german",
+        "private_key_id": "a87e8a6c2da9eb113ee02ce1e332765f6da36e78",
+        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQC8Vt6e2qFOQ7jE\nyIGsGjloQezD14WIFe+or6sq9BzE5Ex8Ou4bKLGxQmOAlAmlc4xY4d2in7MOcFfS\nqfQlBWNQZZU0IuWdlYged0hhjCZ1iHtK4jiv5v3wdM0keZp8eVXMf4UPAIlhrlTb\nRl+C84r7R3UC2ItfXAO1ZEHmNzCETfNC71LwRoTfenO+jHkxaI/4wmc+mU6SGBMv\nEbT+jUeX1wWshuSohzacSJGNQFry8RvuJbOUQ1JXIi786puOBsX3TL73foWUPbq+\nAjl0iybZ4lacivhEKb3HhKAyvwziVElP3htybPWIOdoax2dloDuMT1Mn276p7nGd\nwNbrLjtxAgMBAAECggEABoCeobASrve6QrEEKRj5WmwhlzFdtDEYW86U2PV3TiTu\nWcD/Gi5G1EPig5HuM1Ql8vukAfVZjT9mXijtUPSP/+VB8tGG8/CHCTuSYtZ1OCGa\nKspcrVYK9mj8nCxxSlLgxMqyTAOiD/P2N2Ev8v7NkZdfUYCAb1kR7QqlFnmupyKq\njROfigrmRBJAXQ1kjaFTcmG3p8DVDygn6FMUHOia0/7Cpl3RiI2Sgr79/ran2qGV\n3fZZJTBfl5yD8ieNJ0wt8h/qMlpm/J7ScNdYkKrky/S77/nKkqGQuPCxBenP2aLB\nPzoWn8yFSYOZAjGGyZTI9Ti3Gd05rajHFPLR3JskMQKBgQD4tSqTn+yCOr4FPE3Y\nFh46cCHMzziurWPohneqFiwlIyObl8qYmTXTul03Ui+yEut3+Ts2DzEZWarjTPu8\nTfrYajgIAV+eMhRgxokqU0D/tJqcVHtFSmlFBhYjnBnKQ1A4LeggtQ3OnLd7afAE\n8XvNkcZNvpRTN9+hjUr8dHhw5wKBgQDB3JH9lEMLqQ1kLbfisB83/9JOPO/YKc75\nRlv974oLwoNLVH51RshdkQrMFW2/gi22HEO/CwJLZWR9KskV/V50m1o45GcpkXnc\n8krjrrOV2iHgEM3KAi8fA/MENq7dwxO7OR3ltVKCG4geX/+NPxcEmygi1nXvRIYO\nBWI53mJt5wKBgQDodDvVOtZNaiYMsq53V0TtjbyptszdxsXooTXbTVn5Y/x2Asuj\nESAUQs25VSGrKK/YtKO5BT342aw1QtDO0XuxJqY0JUR/kYMmaKnvW10XInCXyQZm\nXwRBNU0f/1jDiWLC/3VEjLTY9fUwbnWdyS7TlnpoU8ZCeaiVUIBTsXCY5QKBgQCt\n+kQufaMZ0NJKyFjRoNuzhut2+WsJ5KNxxHoET7fky+DGdwKyPx7MEcHN26FNkAdz\n+rO1vKyHe2wOhJpIGMVvAoiarCgE/yCNcAhTgtJ6NnPWQV5d303omMO8UVLnhA2u\nJRk2sF5mk0y6bc7LNQZ0RMZlVXArlaR06tH3OyO8mwKBgQCCCgfPUrOr89wlYWZC\nhClDDI3UYN8U3y4dnQeg7XhE3j7oLBDDPStuB7nWVNdMSw9qMacuK9gAJVC8BZ+n\nd03ogmFhLjeSN5MHsDg9rr0Zspld0WfEFO9eX72fNH/bjMxCbZfSpej7NcB/4LzP\nF/slgroGzBD57TBLk5CZi8Z6OQ==\n-----END PRIVATE KEY-----\n",
+        "client_email": "firebase-adminsdk-fbsvc@quiz-german.iam.gserviceaccount.com",
+        "client_id": "112664334990274609578",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40quiz-german.iam.gserviceaccount.com",
+        "universe_domain": "googleapis.com"
     };
 
     admin.initializeApp({
